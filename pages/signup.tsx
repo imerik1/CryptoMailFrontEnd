2dc32c6108be071aac9024e10ac5fa316966e04d
Erik Santana <erik_s_marques@hotmail.com>
2021-08-23 12:35:35 -0300

    redirect

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -4,6 +4,7 @@ import Head from 'next/head';
 import Form from '../components/formSignup/Form';
 import Header from '../components/header/Header';
 import styles from '../styles/signup.module.scss';
+import parseCookies from '../utils/pasrseCookies';
 
 const SignUp: NextPage = () => {
     return (
@@ -18,6 +19,15 @@ const SignUp: NextPage = () => {
             <footer></footer>
         </>
     );
+};
+
+SignUp.getInitialProps = async ({ req, res }) => {
+    const data = parseCookies(req);
+    if (res) {
+        if (data.user) {
+            res?.writeHead(301, { Location: '/dashboard' }).end();
+        }
+    }
 };
 
 export default SignUp;
