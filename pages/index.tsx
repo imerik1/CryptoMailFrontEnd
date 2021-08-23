50f9e24515d8131e9318694f16f56449697d5944
Erik Santana <erik_s_marques@hotmail.com>
2021-08-23 12:54:29 -0300

    feat: waejewaaew

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import { useRouter } from 'next/dist/client/router';
 import Head from 'next/head';
+import { useCookies } from 'react-cookie';
 
 import Form from '../components/formSignin/Form';
 import Header from '../components/header/Header';
@@ -8,8 +9,12 @@ import styles from '../styles/signin.module.scss';
 import parseCookies from '../utils/pasrseCookies';
 
 const Home: NextPage = ({ data }: any) => {
+    const [cookies, setCookie, removeCookie] = useCookies(['user']);
     const router = useRouter();
     if (data.user) {
+        router.push('/dashboard');
+    }
+    if (cookies.user) {
         router.push('/dashboard');
     }
     return (
